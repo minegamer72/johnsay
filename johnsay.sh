de623619d546f081fa1c69c4c2c0f376ea392a44
bob <108271839+minegamer72@users.noreply.github.com>
2024-09-15 17:07:20 -0400

    fix it

--- johnsay.sh
+++ johnsay.sh
@@ -2,7 +2,7 @@
 
 # method for automatically encoding strings to url format
 urlencode() { # 100% not taken from some repository (i forgot what tho, sorry!)
-  local string="$1" 
+  local string="$@" 
   local length="${#string}" 
   local encoded="" 
   local i # iteration 
@@ -21,7 +21,6 @@ urlencode() { # 100% not taken from some repository (i forgot what tho, sorry!)
 
   echo "$encoded"
 }
-
 # check if any input was passed
 if [ $# -eq 0 ]; then
   echo 'Usage: johnsay "<string>"'
@@ -29,7 +28,7 @@ if [ $# -eq 0 ]; then
 fi
 
 # run the function while passing the string, then set encoded_text to the outputted encoding
-encoded_text=$(urlencode "$1")
+encoded_text=$(urlencode "$@")
 
 curl -s -L "http://tts.cyzon.us/tts?text=$encoded_text" -o johnmadden.wav
 # play it ( made for SoX package )
